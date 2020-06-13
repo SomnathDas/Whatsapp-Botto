28173f85723d25b72b2a566a9ee5f92a46ce4e63
SomnathDas <ak89545@gmail.com>
2020-06-13 09:53:58 +0530

    In Development

--- Bot.py
+++ Bot.py
@@ -1,7 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+import time
 
-PATH = "/home/somnathdas/Whatsapp-Botto/geckodriver"
+PATH = "/home/somnathdas/Whatsapp-Botto/geckodriver" #Add Your own executable path of web driver
 
-driver = webdriver.Firefox(executable_path=PATH)
+driver = webdriver.Firefox(executable_path=PATH) ##Add Your own executable path of web driver
 
+driver.get("https://web.whatsapp.com")
+print("Website Loading Complete, now quitting")
+driver.quit()
