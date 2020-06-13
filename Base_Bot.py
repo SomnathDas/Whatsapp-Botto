0300d210dbae941513c0f7b362c3492fc66b6e96
SomnathDas <ak89545@gmail.com>
2020-06-13 15:24:09 +0530

    In Dev 0.38

--- Base_Bot.py
+++ Base_Bot.py
@@ -15,27 +15,27 @@ driver = webdriver.Firefox(executable_path=PATH) ##Add Your own executable path 
 driver.get("https://web.whatsapp.com")
 
 # Message that user wants to send
-text_message = "Hello World"
+text_message = input("Enter the text message:\n")
 
 #Group Or Contact Name to send message
-nawa = "Otaku Hub"
+Namae = input("Enter the name of contact or group:\n")
 
-#Bot will print out the login instructions by finding it on start page
-#instructions = driver.find_element_by_xpath("/html/body/div[1]/div/div/div[2]/div[1]/div/div[1]")
-#print(instructions.text)
+#Printing Instructions
+print("!! Scan The QR Code in the opened window !! \n ")
 #We will add instruction on our own!!!!!!!! LATER
 
 #Bot will wait for user to scan the QR code
 try:
-    element = WebDriverWait(driver, 20).until(
+    element = WebDriverWait(driver, 40).until(
         EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div/div/div[3]/div/div[1]/div/label/div/div[2]"))
     )
 except:
+    print("Timeout")
     driver.quit()
 
 #Bot will do some shits
 search = driver.find_element_by_xpath("/html/body/div[1]/div/div/div[3]/div/div[1]/div/label/div/div[2]")
-search.send_keys(nawa)
+search.send_keys(Namae)
 
 #so basically, this code below clicks on the topmost search result so the name we type is case-sensitive XD
 search.send_keys(Keys.RETURN)
@@ -45,6 +45,7 @@ msg = driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/footer/
 msg.click() 
 msg.send_keys(text_message)
 msg.send_keys(Keys.RETURN)
+
 print("Complete")
 
 
